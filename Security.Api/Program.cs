1e6ca9cd95257320aeb64f11542ae9a4d41a3722
Louis-Axel Ambroise <LouisAxel.Ambroise@be.zetes.com>
2023-08-23 15:13:31 +0200

    Use default Files

--- Security.Api/Program.cs
+++ Security.Api/Program.cs
@@ -3,7 +3,7 @@ const string FileName = "registered.ip";
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseStaticFiles();
+app.UseDefaultFiles().UseStaticFiles();
 app.MapGet("/register", (HttpContext ctx) => File.WriteAllText(FileName, ctx.Connection.RemoteIpAddress?.ToString()));
 app.MapGet("/live", () => Results.Redirect($"http://{File.ReadAllText(FileName)}:8081"));
 app.MapGet("/ip", () => Results.Ok(File.ReadAllText(FileName)));
