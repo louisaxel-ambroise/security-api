6d388467207aefa4c6582bf025d9f352b9c47fb7
Louis-Axel Ambroise <LouisAxel.Ambroise@be.zetes.com>
2023-08-23 15:02:45 +0200

    Add Home page

--- Security.Api/Program.cs
+++ Security.Api/Program.cs
@@ -1,10 +1,10 @@
+const string FileName = "registered.ip";
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string FileName = "registered.ip";
-
+app.UseStaticFiles();
 app.MapGet("/register", (HttpContext ctx) => File.WriteAllText(FileName, ctx.Connection.RemoteIpAddress?.ToString()));
 app.MapGet("/live", () => Results.Redirect($"http://{File.ReadAllText(FileName)}:8081"));
 app.MapGet("/ip", () => Results.Ok(File.ReadAllText(FileName)));
-
 app.Run();
